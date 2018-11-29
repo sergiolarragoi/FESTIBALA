4bad91904bc9ac40f55b3a5bd22cd1aeee6532b9
ikaslea <ikaslea@ethazi07>
2018-11-29 13:18:54 +0100

    añadir nuevo festival acabado.

--- src/Festival.java
+++ src/Festival.java
@@ -5,94 +5,121 @@ import java.util.Scanner;
 
 public class Festival {
 
-	private String name;
-	private String sponsor;
-	ArrayList<Performance> performances;
+    private String name;
+    private String sponsor;
+    ArrayList<Performance> performances;
 
-	public Festival() {
-		super();
-		this.performances = new ArrayList<Performance>();
-	}
+    public Festival() {
+        super();
+        this.performances = new ArrayList<Performance>();
+    }
 
-	public Festival(String name, String sponsor, ArrayList<Performance> performances) {
-		super();
-		this.name = name;
-		this.sponsor = sponsor;
-		this.performances = new ArrayList<Performance>();
-		this.performances = performances;
-	}
+    public Festival(String name, String sponsor, ArrayList<Performance> performances) {
+        super();
+        this.name = name;
+        this.sponsor = sponsor;
+        this.performances = new ArrayList<Performance>();
+        this.performances = performances;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getSponsor() {
-		return sponsor;
-	}
+    public String getSponsor() {
+        return sponsor;
+    }
 
-	public void setSponsor(String sponsor) {
-		this.sponsor = sponsor;
-	}
+    public void setSponsor(String sponsor) {
+        this.sponsor = sponsor;
+    }
 
-	public ArrayList<Performance> getPerformances() {
-		return performances;
-	}
+    public ArrayList<Performance> getPerformances() {
+        return performances;
+    }
 
-	public void setPerformances(ArrayList<Performance> performances) {
-		this.performances = performances;
-	}
+    public void setPerformances(ArrayList<Performance> performances) {
+        this.performances = performances;
+    }
 
-	public void load_data(String path) {
+    public void load_data(String path) {
 
-		File file = new File(path);
-		Scanner scan = null;
-		Performance performance;
+        File file = new File(path);
+        Scanner scan = null;
+        Performance performance;
 
-		try {
+        try {
 
-			scan = new Scanner(file);
+            scan = new Scanner(file);
 
-			while (scan.hasNextLine()) {
+            while (scan.hasNextLine()) {
 
-				String line = scan.nextLine();
+                String line = scan.nextLine();
 
-				String[] parts = line.split("::");
+                String[] parts = line.split("::");
 
-				performance = new Performance();
+                performance = new Performance();
 
-				performance.setGroupName(parts[0]);
-				performance.setMembers(Integer.parseInt(parts[1]));
-				performance.setDuration(Integer.parseInt(parts[2]));
+                performance.setGroupName(parts[0]);
+                performance.setMembers(Integer.parseInt(parts[1]));
+                performance.setDuration(Integer.parseInt(parts[2]));
 
-				this.performances.add(performance);
-			}
+                this.performances.add(performance);
+            }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	public void printFestivalData() {
+    public void printFestivalData() {
 
-		System.out.println("****************************************************");
-		System.out.println("\n\t FESTIVAL " + this.name);
-		System.out.println("\t Sponsor " + this.sponsor);
+        System.out.println("****************************************************");
+        System.out.println("\n\t FESTIVAL " + this.name);
+        System.out.println("\t Sponsor " + this.sponsor);
 
-		for (int i = 0; i < this.performances.size(); i++) {
+        for (int i = 0; i < this.performances.size(); i++) {
 
-			this.performances.get(i).printPerformanceData();
-		}
-		System.out.println("****************************************************");
-		
-	}
+            this.performances.get(i).printPerformanceData();
+        }
+        System.out.println("****************************************************");
 
-	
-	
-	
+    }
+
+    public void nuevo() {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.println("\n\n\t\t\tPerformance to add");
+        System.out.print("\n\t\tGroup name: ");
+        String groupName = reader.nextLine();
+        System.out.print("\t\tMembers: ");
+        int members = reader.nextInt();
+        System.out.print("\t\tDuration: ");
+        int duration = reader.nextInt();
+//        System.out.println(groupName + " " + members + " " + duration);
+        Performance performance = new Performance(groupName, members, duration);
+        this.performances.add(performance);
+
+    }
+
+    public void buscar() {
+        Scanner reader = new Scanner(System.in);
+        int pos;
+        System.out.println("\tGroup names that contain: ");
+        String groupName = reader.nextLine();
+        System.out.println(groupName);
+        for (int i = 0; i < this.performances.size(); i++) {
+            if (this.performances.get(i).getGroupName().equalsIgnoreCase(groupName)) {
+                pos = i;
+//                System.out.println(pos);
+                this.performances.get(pos).mostrarDatos();
+            }
+        }
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,86 +5,81 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int opcion = 0;
-		
-		Festival festival = new Festival();
-		festival.setName("BBK LIVE");
-		festival.setSponsor("BBK");
-		festival.load_data("src/files/performances.txt");
-		Scanner scan  = new Scanner(System.in);
-		
-		do {
-			System.out.println("\n\t\tFESTIVAL MENU \n");
-			System.out.println("\n\t1.Festibaleko informazio dena erakutsi\n\tMostrar toda la info del festival");
-			System.out.println("\n\t2.Aktuazio berri bat gehitu\n\tA�adir actuacion al festival");
-			System.out.println("\n\t3.Festibalaren iraupena atera kontuan izanda taldeen artean 15 minutuko atsedenaldia dagoela"
-					+ "\n\tTeniendo en cuenta que entre grupo y grupo hay un intervalo de 15mins, mostrar la duracion del festival hay un intervalo de 15mins, mostrar la duracion del festival");
-			System.out.println("\n\t4.Erabilzaileari testu kate bat eskatu eta izenean letra hoiek dituzten aktuazioak atera ( maiuskulak / minuskulak kontuan izan Gabe)"
-					+ "\n\tPedir una cadena de texto por pantalla y mostrar las actuaciones que tengan esa cadena en el nombre, indistintamente de que el usuario haya metido mayusculas o minusculas");
-			System.out.println("\n\t5.100 minuto baino motzagoak diren aktuazio denei 10 min gehitu, eta esan zenbat izan diren"
-					+ "\n\tPara todas las duraciones menores de 100 mins, sumarles 10misn a cada una y mostrar cu�ntos son");
-			System.out.println("\n\t6.Atera iraupen gehien duen aktuazioaren datuak. (bat baino gehiago badira atera denak ) "
-					+ "\n\t Hallar los datos de la actuaci�n con m�s larga. Si hay varias iguales, sacar todas");
-			System.out.println("\n\t7.Irten / Salir");
+        int opcion = 0;
 
-			System.out.print("\n\n\tEnter an option (1-7) 8.End : ");
-			opcion = Integer.parseInt(scan.nextLine());
-			
-			switch (opcion) {
+        Festival festival = new Festival();
+        festival.setName("BBK LIVE");
+        festival.setSponsor("BBK");
+        festival.load_data("src/files/performances.txt");
+        Scanner scan = new Scanner(System.in);
 
-			case 1:
-				//Festibaleko informazio dena erakutsi 
-				//Mostrar toda la info del festival
-				
-				festival.printFestivalData();
-				break;
+        do {
+            System.out.println("\n\t\tFESTIVAL MENU \n");
+            System.out.println("\n\t1.Festibaleko informazio dena erakutsi\n\tMostrar toda la info del festival");
+            System.out.println("\n\t2.Aktuazio berri bat gehitu\n\tA�adir actuacion al festival");
+            System.out.println("\n\t3.Festibalaren iraupena atera kontuan izanda taldeen artean 15 minutuko atsedenaldia dagoela"
+                    + "\n\tTeniendo en cuenta que entre grupo y grupo hay un intervalo de 15mins, mostrar la duracion del festival hay un intervalo de 15mins, mostrar la duracion del festival");
+            System.out.println("\n\t4.Erabilzaileari testu kate bat eskatu eta izenean letra hoiek dituzten aktuazioak atera ( maiuskulak / minuskulak kontuan izan Gabe)"
+                    + "\n\tPedir una cadena de texto por pantalla y mostrar las actuaciones que tengan esa cadena en el nombre, indistintamente de que el usuario haya metido mayusculas o minusculas");
+            System.out.println("\n\t5.100 minuto baino motzagoak diren aktuazio denei 10 min gehitu, eta esan zenbat izan diren"
+                    + "\n\tPara todas las duraciones menores de 100 mins, sumarles 10misn a cada una y mostrar cu�ntos son");
+            System.out.println("\n\t6.Atera iraupen gehien duen aktuazioaren datuak. (bat baino gehiago badira atera denak ) "
+                    + "\n\t Hallar los datos de la actuaci�n con m�s larga. Si hay varias iguales, sacar todas");
+            System.out.println("\n\t7.Irten / Salir");
 
-			case 2:
-				//Aktuazio berri bat gehitu 
-				//A�adir actuacion al festival
-				
-				
-				break;
+            System.out.print("\n\n\tEnter an option (1-7) 8.End : ");
+            opcion = Integer.parseInt(scan.nextLine());
 
-			case 3:
-				//Festibalaren iraupena atera kontuan izanda taldeen artean 15 minutuko atsedenaldia dagoela
-				// Teniendo en cuenta que entre grupo y grupo hay un intervalo de 15mins, mostrar la duracion del festival hay un intervalo de 15mins, mostrar la duracion del festival
-				
-				
-				
-				break;
-			case 4:
-				//Erabilzaileari testu kate bat eskatu eta izenean letra hoiek dituzten aktuazioak atera ( maiuskulak / minuskulak kontuan izan Gabe
-				// Pedir una cadena de texto por pantalla y mostrar las actuaciones que tengan esa cadena en el nombre, indistintamente de que el usuario haya metido mayusculas o minusculas
-				
-				
-				break;
-			case 5:
-				// 100 minuto baino motzagoak diren aktuazio denei 10 min gehitu, eta esan zenbat izan diren
-				// Para todas las duraciones menores de 100 mins, sumarles 10misn a cada una y mostrar cu�ntos son
-				
-				
-				break;
-				
-			case 6:
-				//Iraupen gehien dituen aktuazioaren datuak. (bat baino gehiago badira atera denak )
-				// Hallar los datos de la actuaci�n m�s larga. Si hay varias iguales, sacar todas
-				
-				
-				break;
-			case 7:
-				System.out.println("Coming out...");
-				break;
-			default:
-				System.out.println("Wrong option (1-6) 7-go out");
+            switch (opcion) {
 
-			}
+                case 1:
+                    //Festibaleko informazio dena erakutsi 
+                    //Mostrar toda la info del festival
 
-		} while (opcion != 7);
-		
-		scan.close();
-	}
-	
+                    festival.printFestivalData();
+                    break;
+
+                case 2:
+                    //Aktuazio berri bat gehitu 
+                    //Añadir actuacion al festival
+
+                    festival.nuevo();
+                    break;
+
+                case 3:
+                    //Festibalaren iraupena atera kontuan izanda taldeen artean 15 minutuko atsedenaldia dagoela
+                    // Teniendo en cuenta que entre grupo y grupo hay un intervalo de 15mins, mostrar la duracion del festival hay un intervalo de 15mins, mostrar la duracion del festival
+
+                    break;
+                case 4:
+                    //Erabilzaileari testu kate bat eskatu eta izenean letra hoiek dituzten aktuazioak atera ( maiuskulak / minuskulak kontuan izan Gabe
+                    // Pedir una cadena de texto por pantalla y mostrar las actuaciones que tengan esa cadena en el nombre, indistintamente de que el usuario haya metido mayusculas o minusculas
+                    festival.buscar();
+                    break;
+                case 5:
+                    // 100 minuto baino motzagoak diren aktuazio denei 10 min gehitu, eta esan zenbat izan diren
+                    // Para todas las duraciones menores de 100 mins, sumarles 10misn a cada una y mostrar cu�ntos son
+
+                    break;
+
+                case 6:
+                    //Iraupen gehien dituen aktuazioaren datuak. (bat baino gehiago badira atera denak )
+                    // Hallar los datos de la actuaci�n m�s larga. Si hay varias iguales, sacar todas
+
+                    break;
+                case 7:
+                    System.out.println("Coming out...");
+                    break;
+                default:
+                    System.out.println("Wrong option (1-6) 7-go out");
+
+            }
+
+        } while (opcion != 7);
+
+        scan.close();
+    }
+
 }
